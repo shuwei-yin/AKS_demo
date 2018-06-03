85dee196294124a8624608c1d41c09349bb102e8
shuweiyin <shuweiyin@ali-C3F545F7-A666-5FD4-93D3-62D07E798C62.local>
2018-06-03 20:40:58 +0100

    test fs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs")
+
+var fs_output_data = ""
+
+/* GET file content*/
+fs.readFile("./test_conf", 'utf-8', function(err, data){
+	if (err) {
+		return console.log(err);
+	}
+	else{
+		fs_output_data = data;
+	}
+
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { name: 'Li Shan' })
+	res.render('index', { name: 'Li Shan', 'fs_output_data': fs_output_data })
 });
 
 module.exports = router;
