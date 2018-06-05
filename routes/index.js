1e9e9d3d270a443c836e16cb9e21b8bb8c13b45f
shuweiyin <shuweiyin@ali-C3F545F7-A666-5FD4-93D3-62D07E798C62.local>
2018-06-05 18:21:23 +0100

    update fileread

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,23 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs")
 
-var json_data = {}
+var json_data
 
 /* GET file content*/
-var readfile_data = fs.readFile("/data/test_conf", 'utf-8', function(err, data){
+fs.readFile("/data/test_conf", 'utf-8', function(err, data){
 	if (err) {
 		return console.log(err);
 	}
 	else{
 		json_data = JSON.parse(data);
 		console.log("username========"+json_data.user_name)
-		return data
 	}
 })
 
-var result_data = JSON.parse(readfile_data)
-console.log("result_data username========"+result_data.user_name)
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { user_name: result_data.user_name, 'password': json_data.password })
+	res.render('index', { user_name: json_data.user_name, 'password': json_data.password })
 });
 
 module.exports = router;
