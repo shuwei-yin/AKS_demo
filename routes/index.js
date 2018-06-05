2397ee1e21071d5b700735dfcd629d1e00c65f5c
shuweiyin <shuweiyin@ali-c3f545f7-a666-5fd4-93d3-62d07e798c62.home>
2018-06-05 20:34:51 +0100

    update readfile

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,18 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs")
 
-var json_data
 
 /* GET file content*/
 fs.readFile("/data/test_conf", 'utf-8', function(err, data){
 	if (err) {
 		return console.log(err);
 	}
-	else{
-		var parse_data = JSON.parse(data);
-		console.log("username========"+parse_data.user_name)
-		/* GET home page. */
-		router.get('/', function(req, res, next) {
-			res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
-		});
-	}
+	var parse_data = JSON.parse(data);
+	console.log("username11========"+parse_data.user_name)
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+		res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
+	});
 })
 
 
