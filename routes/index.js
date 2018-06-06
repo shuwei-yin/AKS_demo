82c0da15babcd4f01aa9dc9a2ac5e4caf03150f8
shuweiyin <shuweiyin@ali-C3F545F7-A666-5FD4-93D3-62D07E798C62.local>
2018-06-06 23:47:29 +0100

    cache

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,25 @@ var express = require('express');
 var router = express.Router();
 var fs = require("fs")
 
+
+/* GET file content*/
+	/* GET home page. */
 router.get('/*', function(req, res, next) {
 	res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", 0);
     res.setHeader('Last-Modified', (new Date()).toUTCString());
-    next();
-});
 
-/* GET file content*/
-fs.readFile("/data/test_conf", 'utf-8', function(err, data){
+	fs.readFile("/data/test_conf", 'utf-8', function(err, data){
 	if (err) {
 		return console.log(err);
 	}
 	var parse_data = JSON.parse(data);
 	var cur_date = new Date().toISOString()
 	console.log("username11========"+parse_data.user_name + "========date:"+ cur_date)
-	/* GET home page. */
-	router.get('/', function(req, res, next) {
-		res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
-	});
-})
+	})
+	res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
+});
 
 
 module.exports = router;
