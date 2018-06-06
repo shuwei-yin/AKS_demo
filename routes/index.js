e8b6728de7f61881293d7e3a101da0068207a9a2
shuweiyin <shuweiyin@ali-C3F545F7-A666-5FD4-93D3-62D07E798C62.local>
2018-06-06 23:55:12 +0100

    cache

--- routes/index.js
+++ routes/index.js
@@ -11,16 +11,18 @@ router.get('/', function(req, res, next) {
     res.setHeader("Expires", 0);
     res.setHeader('Last-Modified', (new Date()).toUTCString());
 
-    var parse_data = JSON.parse(data);
-	var cur_date = new Date().toISOString()
+    
 
 	fs.readFile("/data/test_conf", 'utf-8', function(err, data){
 		if (err) {
 			return console.log(err);
 		}
 		
-		console.log("username11========"+parse_data.user_name + "========date:"+ cur_date)
+		var parse_data = JSON.parse(data);
+		var cur_date = new Date().toISOString()
 		
+		console.log("username11========"+parse_data.user_name + "========date:"+ cur_date)
+
 		res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
 	})
 
