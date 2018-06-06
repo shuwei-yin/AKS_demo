f7c2ef9bf6dd89e3de06b024c160aca2d04c1282
shuweiyin <shuweiyin@ali-C3F545F7-A666-5FD4-93D3-62D07E798C62.local>
2018-06-06 23:50:19 +0100

    cache

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,25 @@ var fs = require("fs")
 
 /* GET file content*/
 	/* GET home page. */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
 	res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.setHeader("Pragma", "no-cache");
     res.setHeader("Expires", 0);
     res.setHeader('Last-Modified', (new Date()).toUTCString());
 
-	fs.readFile("/data/test_conf", 'utf-8', function(err, data){
-	if (err) {
-		return console.log(err);
-	}
-	var parse_data = JSON.parse(data);
+    var parse_data = JSON.parse(data);
 	var cur_date = new Date().toISOString()
-	console.log("username11========"+parse_data.user_name + "========date:"+ cur_date)
+
+	fs.readFile("/data/test_conf", 'utf-8', function(err, data){
+		if (err) {
+			return console.log(err);
+		}
+		
+		console.log("username11========"+parse_data.user_name + "========date:"+ cur_date)
 	})
+	
 	res.render('index', { user_name: parse_data.user_name, 'password': parse_data.password })
+	
 });
 
 
